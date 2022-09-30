f6a9a203d23226f2b964db607b14059c1b02e1d6
Irina Galata <igalata13@gmail.com>
2022-09-30 13:04:47 +0200

    Copied over the changes from the previous chapter

--- 04-creating-animations/projects/final/SportFan/EventDetails/EventDetailsView.swift
+++ 04-creating-animations/projects/final/SportFan/EventDetails/EventDetailsView.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2022 Razeware LLC
-/// 
+///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-/// 
+///
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-/// 
+///
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-/// 
+///
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -49,8 +49,8 @@ struct EventDetailsView: View {
         HeaderView(
           namespace: namespace,
           event: event,
-          collapsed: $collapsed,
-          offset: $offset
+          collapsed: collapsed,
+          offset: offset
         )
         Spacer()
         if collapsed {
@@ -81,7 +81,7 @@ struct EventDetailsView: View {
                 .foregroundColor(.secondary)
                 .padding(.horizontal)
 
-              EventLocationAndDate(collapsed: $collapsed, namespace: namespace, event: event)
+              EventLocationAndDate(collapsed: collapsed, namespace: namespace, event: event)
                 .zIndex(3)
 
               if !collapsed {
@@ -162,7 +162,7 @@ struct EventDetailsView: View {
       Button(action: {
         seatingChartVisible = true
       }, label: {
-        Image(uiImage: UIImage(named: "seats")!)
+        Image("seats")
           .resizable()
           .renderingMode(.template)
           .scaledToFit()
