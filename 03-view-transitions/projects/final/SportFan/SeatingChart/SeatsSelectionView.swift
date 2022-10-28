278fa9b71ba66ccbfbf4d0f74a85038fa0616937
Shai Mishali <freak4pc@gmail.com>
2022-10-28 22:15:19 +0300

    move seating chart to its own folder like ch 04

--- 03-view-transitions/projects/final/SportFan/SeatingChart/SeatsSelectionView.swift
+++ 03-view-transitions/projects/final/SportFan/SeatingChart/SeatsSelectionView.swift
@@ -33,15 +33,62 @@
 import SwiftUI
 
 struct SeatsSelectionView: View {
-  let event: Event
-  
+  var event: Event
+
   var body: some View {
     VStack {
+      VStack {
+        Text(event.team.name)
+          .frame(maxWidth: .infinity, alignment: .leading)
+          .font(.title2)
+          .fontWeight(.black)
+          .foregroundColor(.white)
+          .padding([.top, .horizontal])
+          .shadow(radius: 2)
+          .zIndex(1)
+
+        HStack {
+          Text(event.date)
+            .font(.subheadline)
+            .foregroundColor(.white)
+
+          Spacer()
+
+          Image("cart")
+            .renderingMode(.template)
+            .resizable()
+            .scaledToFit()
+            .frame(height: Constants.iconSizeL)
+            .clipped()
+            .foregroundColor(.white)
+        }.padding(.horizontal)
+          .shadow(radius: 2)
+      }
+
       Spacer()
-      Text("// TODO: Implement seating chart")
-        .frame(maxWidth: .infinity)
-        .padding()
+
+      Text("Something is missing here!")
+
       Spacer()
+
+      Button(
+        action: {
+        },
+        label: {
+          Text("Buy Tickets")
+            .lineLimit(1)
+            .foregroundColor(.black)
+            .frame(maxWidth: .infinity)
+            .frame(height: 48)
+            .background {
+              RoundedRectangle(cornerRadius: 36)
+                .fill(.white)
+                .shadow(radius: 2)
+            }
+            .padding(.horizontal)
+        }
+      )
+      .padding(.vertical, Constants.spacingM)
     }
     .background(Constants.orange, ignoresSafeAreaEdges: .all)
   }
